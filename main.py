573e1ac6b60998d44610cbed1aac789ab748f696
ASWIN0603 <109233413+ASWIN0603@users.noreply.github.com>
2023-05-09 19:38:45 +0530

    commit

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from flask import Flask,render_template,request,session,redirect,url_for,flash
+from flask import Flask,render_template,request,session,redirect,url_for,flash, make_response, send_file
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
-from flask_login import login_required,current_user
-import json
+from reportlab.lib.pagesizes import A4, portrait
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, Table, TableStyle
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.units import inch
+from reportlab.pdfgen import canvas
+from reportlab.lib.utils import ImageReader
+import datetime
 
 app = Flask(__name__)
 
@@ -13,13 +17,14 @@ app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:@localhost/studentprofile'
 db=SQLAlchemy(app)
 
 #Table for studnt details
-class year(db.Model):
-    SL_NO = db.Column(db.Integer, primary_key=True)
+class student(db.Model):
+    SI_NO = db.Column(db.Integer, primary_key=True)
     APPL_NO = db.Column(db.String(50))
     ROLL_NO = db.Column(db.Integer)
     NAME = db.Column(db.String(50)) 
     DOB_YYYY_MM_DD  = db.Column(db.String(50))  
     GENDER = db.Column(db.String(50))
+    YEAR = db.Column(db.String(50)) 
     DEGREE = db.Column(db.String(50))
     BRANCH  = db.Column(db.String(50))
     MODE = db.Column(db.String(50)) 
@@ -105,8 +110,11 @@ class year(db.Model):
     NO_OF_ARREARS = db.Column(db.Integer) 
     PLACEMENT_STATUS = db.Column(db.String(100))
 
-class consumable_product(db.Model):
-    PRODUCT_ID = db.Column(db.Integer, primary_key=True)
+class stock_management(db.Model):
+    SI_NO = db.Column(db.Integer, primary_key=True)
+    STOCK_TYPE = db.Column(db.String(100)) 
+    DEPARTMENT = db.Column(db.String(100)) 
+    PRODUCT_ID = db.Column(db.Integer)
     DATE_OF_PURCHASE = db.Column(db.String(100)) 
     BILL_INVOICE_NO = db.Column(db.String(100)) 
     BILL_DATE = db.Column(db.String(100)) 
@@ -143,7 +151,8 @@ def nonconsumable():
 
 #admin credentials
 admindata = {'stock@ucen': '123',
-            'scholarship@ucen': '123', }
+            'scholarship@ucen': '123',
+            'bonafide@ucen':'123'}
 
 #Admin authetication
 @app.route('/admin_login', methods=['POST', 'GET'])
@@ -153,19 +162,19 @@ def login():
     if name not in admindata:
         return render_template('login.html',
                                info='Invalid User !')
-    
     elif admindata[name] != pwd:
          return render_template('login.html',
                                    info='Invalid Password !')
-            
-    elif admindata[name] == admindata['stock@ucen']:
-        
-            return render_template('stock.html')
-         
+    elif name == 'stock@ucen':
+        return render_template('stock.html')
+    elif name == 'bonafide@ucen':
+        return render_template('bonafide.html')
+    elif name == 'scholarship@ucen':
+        return render_template('scholarship.html')
     else:
-       if admindata[name] == admindata['scholarship@ucen']:
-           
-           return render_template('scholarship.html')
+        return render_template('login.html',
+                               info='Invalid User !')
+
        
 #faculty credentials
 facultydata = {
@@ -200,10 +209,10 @@ def faculty_login():
 def student_login():
     name = request.form['studentusername']
     pwd = request.form['studentpassword']
-    student = year.query.filter_by(ROLL_NO=name, DOB_YYYY_MM_DD=pwd).first()
+    student = student.query.filter_by(ROLL_NO=name, DOB_YYYY_MM_DD=pwd).first()
     
     if student:
-      query = year.query.filter_by(ROLL_NO=name).first()      
+      query = student.query.filter_by(ROLL_NO=name).first()      
       return render_template('studentprofileview.html',query=query)
     else:
        return render_template('login.html')
@@ -213,26 +222,26 @@ def student_login():
 @app.route('/search', methods=['POST', 'GET'])
 def search_student():
   reg = request.form['roll']
-  search=year.query.filter_by(ROLL_NO=reg).first()
+  search=student.query.filter_by(ROLL_NO=reg).first()
   return render_template('admin.html',search=search)
 
 #edit student details by faculties
 @app.route('/edit/<int:ROLL_NO>', methods=['GET', 'POST'])
 def edit(ROLL_NO):
-    post = year.query.filter_by(ROLL_NO=ROLL_NO).first()
+    post = student.query.filter_by(ROLL_NO=ROLL_NO).first()
     if request.method == 'POST':
         post.APPL_NO = request.form['appl_no']
         post.ROLL_NO = request.form['roll_no']
         post.NAME = request.form['name']
         db.session.commit()
-        flash('Year record updated successfully!', 'success')
-    posts=year.query.filter_by(ROLL_NO=ROLL_NO).first()
+        flash('student record updated successfully!', 'success')
+    posts=student.query.filter_by(ROLL_NO=ROLL_NO).first()
     return render_template('edit.html', post=post,posts=posts)
 
 #view all student details of their department
 @app.route('/studentdetails')
 def studentdetails():
-    query=year.query.all() 
+    query=student.query.all() 
     return render_template('studentdetails.html',query=query)
 
 
@@ -246,22 +255,22 @@ def update():
 @app.route('/updatearrear', methods=['GET', 'POST'])
 def updatearrear():
     regnum=request.form['regnum']
-    post = year.query.filter_by(ROLL_NO=regnum).first()
+    post = student.query.filter_by(ROLL_NO=regnum).first()
     if request.method == 'POST':
         post.NO_OF_ARREARS = request.form['arrear']
         db.session.commit()
-        flash('Year record updated successfully!', 'success')
+        flash('student record updated successfully!', 'success')
     return render_template('update.html', post=post)
 
 #update placement
 @app.route('/updateplacement', methods=['GET', 'POST'])
 def updateplacement():
     regnum=request.form['regnum']
-    post = year.query.filter_by(ROLL_NO=regnum).first()
+    post = student.query.filter_by(ROLL_NO=regnum).first()
     if request.method == 'POST':
         post.PLACEMENT_STATUS = request.form['placement']
         db.session.commit()
-        flash('Year record updated successfully!', 'success')
+        flash('student record updated successfully!', 'success')
     return render_template('update.html', post=post)
 
 
@@ -269,7 +278,7 @@ def updateplacement():
 
 @app.route('/consumable')
 def consumable():
-    query = consumable_product.query.all()
+    query = stock_management.query.all()
     return render_template("consumable.html",query=query)
 
 
@@ -278,6 +287,10 @@ def consumable():
 def add_product():
     if request.method == 'POST':
         # retrieve form data from request object
+        si_no = max(stock_management.SI_NO) +1
+        stock_type = request.form['stock_type']
+        department = request.form['department']
+        product_id = request.form['product_id']
         date_of_purchase = request.form['date_of_purchase']
         bill_invoice_no = request.form['bill_invoice_no']
         bill_date = request.form['bill_date']
@@ -291,20 +304,84 @@ def add_product():
         total_stock = int(request.form['total_stock'])
         condition = request.form['condition']
         location = request.form['location']
-        transfer_history = request.form['transfer_history']
         remarks = request.form['remarks']
         
         # create new consumable product object
-        new_product = consumable_product(DATE_OF_PURCHASE=date_of_purchase, BILL_INVOICE_NO=bill_invoice_no, BILL_DATE=bill_date,
+        new_product = stock_management(SI_NO=si_no,STOCK_TYPE=stock_type,DEPARTMENT=department,PRODUCT_ID=product_id,DATE_OF_PURCHASE=date_of_purchase, BILL_INVOICE_NO=bill_invoice_no, BILL_DATE=bill_date,
                                           EQUIPMENT=equipment, NO_OF_QUANTITY=no_of_quantity, COST_PER_UNIT=cost_per_unit,
                                           TOTAL_COST=total_cost, SUPPLIER_NAME=supplier_name, WARRANTY=warranty, WARRANTY_PERIOD=warranty_period,
-                                          TOTAL_STOCK=total_stock, CONDITION=condition, LOCATION=location,
-                                          TRANSFER_HISTORY=transfer_history, REMARKS=remarks)
+                                          TOTAL_STOCK=total_stock, CONDITION=condition, LOCATION=location, REMARKS=remarks)
         
         # add the new product to the database session and commit the changes
         db.session.add(new_product)
         db.session.commit()
+        return render_template("addstock.html")
+    
+    
+#bonafide generation
+@app.route('/generate_bonafide', methods=['POST'])
+def generate_bonafide():
+    # Get the user's input from the HTML form
+    rollno = request.form['rollno']
+    post = student.query.filter_by(ROLL_NO=rollno).first()
+    name = post.NAME
+    refid = request.form['refid']
+    date = datetime.date.today()
+    formatted_date = date.strftime("%d-%m-%Y")
+    gender = post.GENDER
+    if gender == 'MALE':
+        salutation = 'Mr.'
+        pronoun = 'He'
+    else:
+        salutation = 'Mrs.'
+        pronoun = 'She'
 
+    father_name = post.FATHER_NAME
+    mother_name = post.MOTHER_NAME
+    year = post.YEAR
+    degree = post.DEGREE
+    department = post.BRANCH
+    current_year = datetime.datetime.now().year
+    next_year = current_year + 1
+
+    # Create a new PDF document
+    filename = f"{name}_bonafide.pdf"
+    document = canvas.Canvas(filename, pagesize=A4)
+
+    # Set the font and font size
+    document.setFont("Times-Bold", 14)
+    
+    logo_path = 'static/aulogo.png'
+    logo = ImageReader(logo_path)
+    document.drawImage(logo, 30, 780, width=50, height=50)
+
+    # Add the content
+    document.drawString(130, 810, "UNIVERSITY COLLEGE OF ENGINEERING, NAGERCOIL")
+    document.drawString(160, 790, "(A Constituent College Of Anna University, Chennai)")
+    document.drawString(0, 770, "______________________________________________________________________________________________________")
+    document.drawString(30, 755, "Dr. V.A. Nagarajan")
+    document.drawString(30, 740, "DEAN")
+    document.drawString(410, 755, "Phone: 04652-260510")
+    document.drawString(410, 740, "E-Mail: deanucen@gmail.com")
+    document.drawString(0, 735, "______________________________________________________________________________________________________")
+    document.drawString(30, 715, f"Ref : {refid}")
+    document.drawString(430, 715, f"Date : {formatted_date}")
+    document.drawString(230, 660, "BONAFIDE CERTIFICATE")
+    document.setFont("Times-Roman", 14)
+    document.drawString(80, 630, f"This is to certify that {salutation} {name}, (Reg. No: {rollno})")
+    document.drawString(30, 610, f"S/o. Mr. {father_name} & Mrs. {mother_name} is a bonafide student of")
+    document.drawString(30, 590, f"University College of Engineering, Nagercoil. {pronoun} Studies {year} Year {degree} in the Department of ")
+    document.drawString(30, 570, f"{department} during the academic year {current_year} - {next_year} ")
+    
+    # Save the PDF document
+    document.showPage()
+    document.save()
+
+    # Serve the PDF document as a response to the user
+    response = make_response(open(filename, 'rb').read())
+    response.headers.set('Content-Type', 'application/pdf')
+    response.headers.set('Content-Disposition', 'attachment', filename=filename)
+    return response
 
 
 if __name__ == '__main__':
